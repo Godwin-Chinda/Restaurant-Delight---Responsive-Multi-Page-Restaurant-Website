e938b8b7c2931431c972146631f8beac82bb1fab
Godwin-Chinda <godwinbadubulachim@gmail.com>
2025-04-05 00:28:08 +0100

    Added About page

--- landing-page.js
+++ landing-page.js
@@ -1,13 +1,12 @@
-const menuToggle = document.querySelector('.menu-toggle'); 
-const navLinks = document.querySelector('.nav-links');
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
+const exploreBtn = document.getElementById("explore-btn");
 
-const exploreBtn = document.getElementById('explore-btn');
-
-exploreBtn.addEventListener('click', () => {
-    document.querySelector('#menu').scrollIntoView({ behavior: 'smooth' });
+exploreBtn.addEventListener("click", () => {
+  document.querySelector("#menu").scrollIntoView({ behavior: "smooth" });
 });
